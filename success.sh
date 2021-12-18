52fa5dc8cdb26cbce6b67b7859fbef199b931edb
Zack Stevens <zack.st7@gmail.com>
2021-12-17 20:29:59 -0700

    fix: init

--- success.sh
+++ success.sh
@@ -3,14 +3,6 @@ set -e
 
 newTag=$1
 majorTag="v$(echo $newTag | grep -o '[^-]*$' | cut -d. -f1)"
-echo "new tag is $newTag"
-echo "major tag is $majorTag"
-
-# delete current major tag, pipe true in case it doesn't exist
-echo "deleting local tag"
 git tag -d $majorTag || true
-echo "deleting remote tag"
 git push -d origin $majorTag || true
-# create new tag
-echo "duplicating tag $newTag as $majorTag"
 git tag $majorTag
